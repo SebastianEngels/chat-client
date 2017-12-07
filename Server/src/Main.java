98cd2643ac4220c023aec6b93eb8f42f983364ef
Seppel <Seppel@SEPPEL-PC>
2017-12-07 20:45:26 +0100

    Added Threaded Socket listeners, added fancy Console

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+
 import model.Connection;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Connection connection = new Connection("localhost",6666);
-		System.out.println(connection.start());
+		Connection connection = new Connection(5666, 5);
+		try {
+			System.out.println(connection.start());
+		} catch (IOException e) {
+			System.out.println("[Server] " + e.getMessage());
+		}
 	}
 
 }
--- Server/src/model/Connection.java
+++ Server/src/model/Connection.java
@@ -1,33 +1,54 @@
 package model;
 
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
 
 public class Connection {
+
+
 	private int port;
 	private int timeout = 100;
+	private int numOfThreads = 10;
 	private Socket socket = new Socket();
 	private ServerSocket serverSocket;
 	
-	public Connection(String host, int port, int timeout) {
-		this.port = port;
-		this.timeout = timeout;
-	}
-
-	public Connection(String host, int port) {
+	public Connection(int port) {
 		this.port = port;
 	}
 	
-	public String start() {
-		try {
-			serverSocket = new ServerSocket(port);
-			Socket client = serverSocket.accept();
-		} catch (IOException e) {
-			return "[ERROR] " + e.getMessage();
-		}
+	public Connection(int port, int numOfThreads) {
+		this.port = port;
+		this.numOfThreads = numOfThreads;
+	}
+	
+	public Connection(int port, int timeout, int numOfThreads) {
+		this.port = port;
+		this.timeout = timeout;
+		this.numOfThreads = numOfThreads;
+	}
+	
+
+	
+	
+	public String start() throws IOException{
+		final ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
+		serverSocket = new ServerSocket(port);
 		
-		return "[Server: " + serverSocket.getInetAddress() + "] Server Started.";
+		while (true) {
+			executor.execute(new Runnable() {
+				@Override public void run(){
+					try {
+						System.out.println("[Server] |" + Thread.currentThread().getName() + "| waiting for a Connection");
+						Socket client = serverSocket.accept();
+						System.out.println("[Server] |" + Thread.currentThread().getName() + "| received Data from " + client.getInetAddress());
+					} catch (IOException e) {
+						System.out.println("[Server] |" + Thread.currentThread().getName() + "| " + e.getMessage());
+					}
+				}
+			});
+		}
 	}
 	
 	
@@ -45,6 +66,14 @@ public class Connection {
 
 	public void setTimeout(int timeout) {
 		this.timeout = timeout;
+	}
+
+	public int getNumOfThreads() {
+		return numOfThreads;
+	}
+
+	public void setNumOfThreads(int numOfThreads) {
+		this.numOfThreads = numOfThreads;
 	}
 
 	public Socket getSocket() {
